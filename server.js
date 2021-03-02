ba99fafd9156106bcc484d37f195f32bb512f789
David-Werth <75678655+David-Werth@users.noreply.github.com>
2021-03-02 15:11:51 +0100

    connected database

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const image = require("./controllers/image");
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'test',
-        database: 'smart-brain'
+        host: 'postgresql-solid-20078',
+        user: 'rzfeujbcywvrhd',
+        password: '',
+        database: 'still-everglades-61678'
     }
 });
 
